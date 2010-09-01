fdad6d8fad600532502e663786419621538706de
rasmusrn <rasmusrnielsen@gmail.com>
2010-09-02 00:02:13 +0200

    Added required module Observable.

---
+++ src/lib/observable.js
@@ -0,0 +1,31 @@
+var Observable = {
+	subscribe: function(event, callback) {
+		this.observers = this.observers || {};
+		this.observers[event] = this.observers[event] || [];
+		this.observers[event].push(callback);
+	},
+	unsubscribe: function(event, callback) {
+		this.observers[event] = $.grep(this.observers[event], function(e) {
+			return e != callback;
+		});
+	},
+	notify: function() {
+		if(this.observers) {
+			var event = arguments[0];
+			var observers = this.observers[event];
+			if(observers) {
+				var passing_arguments = this.argumentsToArray(arguments, 1);
+				$.each(observers, function(i, observer) {
+					observer.apply(null, passing_arguments);
+				});
+			}
+		}
+	},
+	argumentsToArray: function(arguments, start_from) {
+		var array = [];
+		for(var i=(start_from || 0); arguments.length>i; i++) {
+			array.push(arguments[i]);
+		}
+		return array;
+	}
+};
