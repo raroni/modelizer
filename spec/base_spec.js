b934996caa9c0a6a6049cb3ebaaee813616e4e41
rasmusrn <rasmusrnielsen@gmail.com>
2010-10-08 15:24:36 +0200

    Modelizer now throws and exception if you try to instantiate to models with same ID.

---
+++ spec/base_spec.js
@@ -0,0 +1,38 @@
+describe('Modelizer.Base', function() {
+  Modelizer('User');
+  
+  var user, user_name = 'Rasmus';
+  
+  beforeEach(function() {
+    user = new User({ name: user_name });
+  });
+  
+  it('should be able to get an attribute', function() {
+    expect(user.get('name')).toEqual(user_name);
+  });
+  
+  it('should mark changed attribute (updated via #update) as changed', function() {
+    user.update({ age: 24, name: 'John' });
+    expect(user.changed('name')).toBeTruthy();
+  });
+  
+  it('should mark changed attribute (updated via #set) as changed', function() {
+    user.set('age', 24);
+    expect(user.changed('age')).toBeTruthy();
+  });
+  
+  it('should mark not updated attributes as not changed', function() {
+    user.set('age', 24);
+    expect(user.changed('name')).toBeFalsy();
+  });
+  
+  it('should not mark a unchanged attribute as changed', function() {
+    user.update({ age: 24 });
+    expect(user.changed('name')).toBeFalsy();
+  });
+  
+  it('should mark instantiation attributes as changed', function() {
+    expect(user.changed('name')).toBeTruthy();
+  });
+
+});
