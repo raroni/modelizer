b5de29c9f6b2ef3ef4758053d497ca4b911c21b4
rasmusrn <rasmusrnielsen@gmail.com>
2010-10-08 11:00:36 +0200

    Updated some tests.

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -11,9 +11,19 @@ describe('Modelizer', function() {
     expect(user.get('name')).toEqual(user_name);
   });
   
-  it('should mark a changed attribute as changed', function() {
+  it('should mark changed attribute (updated via #update) as changed', function() {
     user.update({ age: 24, name: 'John' });
     expect(user.changed('name')).toBeTruthy();
+  });
+  
+  it('should mark changed attribute (updated via #set) as changed', function() {
+    user.set('age', 24);
+    expect(user.changed('age')).toBeTruthy();
+  });
+  
+  it('should mark not updated attributes as not changed', function() {
+    user.set('age', 24);
+    expect(user.changed('name')).toBeFalsy();
   });
   
   it('should not mark a unchanged attribute as changed', function() {
